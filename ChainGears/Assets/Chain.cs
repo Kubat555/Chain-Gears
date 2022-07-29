01f951db0548f0153d9b837de1ade804cfba80b1
@Kubat555 <kubat.chyngyzov@gmail.com>
2022-07-29 18:56:18 +0600

    fix bugs, but have many other bugs, because i am Chines

--- ChainGears/Assets/Chain.cs
+++ ChainGears/Assets/Chain.cs
@@ -7,11 +7,12 @@ public class Chain : MonoBehaviour
     [SerializeField] List<ChainController> chainParts;
     public List<ChainController> chains {get{return chainParts;} private set{}}
     public static Chain instance;
-    public float radiusForSpawn;
 
     Ray ray;
     int index = 0;
     bool firstChainPart;
+    bool canConnection;
+    bool chainIsDone;
 
     private void Awake() {
         if(instance == null){
@@ -19,32 +20,63 @@ public class Chain : MonoBehaviour
         }
         index = 0;
         firstChainPart = true;
+        canConnection = false;
+        chainIsDone = false;
     }
 
-    private void Update() {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    }
+
 
     void FixedUpdate()
     {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if(Input.GetMouseButton(0) && firstChainPart){
             if(Physics.Raycast(ray, out RaycastHit raycastHit)){
                 chainParts[index].transform.position = new Vector3(raycastHit.point.x, 0.6f, raycastHit.point.z);
             }
             firstChainPart = false;
         }
-        else if(Input.GetMouseButton(0)){
+        else if(Input.GetMouseButton(0) && !chainIsDone){
             
             if(Physics.Raycast(ray, out RaycastHit raycastHit)){
-                // chainParts[index].transform.position = raycastHit.point;
+                // chainParts[index].transform.LookAt(raycastHit.point, Vector3.right);
+                if(canConnection){
+                    if((Mathf.Abs(chainParts[index].transform.position.z - raycastHit.point.z) > chainParts[index].radiusForSpawn || Mathf.Abs(chainParts[index].transform.position.x - raycastHit.point.x) > chainParts[index].radiusForSpawn) && (Mathf.Abs(chainParts[0].transform.position.z - raycastHit.point.z) < chainParts[0].radiusForSpawn || Mathf.Abs(chainParts[0].transform.position.x - raycastHit.point.x) < chainParts[0].radiusForSpawn)){
 
-                if(Mathf.Abs(chainParts[index].transform.position.z - raycastHit.point.z) > radiusForSpawn || Mathf.Abs(chainParts[index].transform.position.x - raycastHit.point.x) > radiusForSpawn){
+                        index = Mathf.Clamp(index + 1, 0, chainParts.Count - 1);
+                        chainParts[index].transform.position = new Vector3(raycastHit.point.x, 0.6f, raycastHit.point.z);
+
+                        HingeJoint joint = chainParts[0].gameObject.AddComponent<HingeJoint>();
+                        joint.connectedBody =  chainParts[index].rb;
+                        joint.useSpring = true;
+                        JointSpring hingeSpring = joint.spring;
+                        hingeSpring.spring = 30;
+                        hingeSpring.damper = 10;
+                        joint.spring = hingeSpring;
+                        canConnection = true;
+                        chainIsDone = true;
+                    }
+                }
+
+
+                else if(Mathf.Abs(chainParts[index].transform.position.z - raycastHit.point.z) > chainParts[index].radiusForSpawn || Mathf.Abs(chainParts[index].transform.position.x - raycastHit.point.x) > chainParts[index].radiusForSpawn){
                     index = Mathf.Clamp(index + 1, 0, chainParts.Count - 1);
                     chainParts[index].transform.position = new Vector3(raycastHit.point.x, 0.6f, raycastHit.point.z);
+                    ConfigureJointChainPart();
                 }
             }
             
         }
+    }
+
+    void ConfigureJointChainPart(){
+        HingeJoint joint = chainParts[index].gameObject.AddComponent<HingeJoint>();
+        joint.connectedBody =  chainParts[index - 1].rb;
+        joint.useSpring = true;
+        JointSpring hingeSpring = joint.spring;
+        hingeSpring.spring = 30;
+        hingeSpring.damper = 10;
+        joint.spring = hingeSpring;
+        canConnection = true;
     }
 
 }
--- ChainGears/Assets/Scripts/ChainController.cs
+++ ChainGears/Assets/Scripts/ChainController.cs
@@ -8,14 +8,17 @@ public class ChainController : MonoBehaviour
     // [SerializeField] GameObject chain;
     // [SerializeField] bool isHead = false;
     // static ChainController activeChain;
+    public float radiusForSpawn;
 
-    
+    [HideInInspector]
+    public Rigidbody rb;
     private void Start() {
-        
+        rb = GetComponent<Rigidbody>();
     }
+    
     
     private void OnDrawGizmosSelected() {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, Chain.instance.radiusForSpawn);
+        Gizmos.DrawWireSphere(transform.position, radiusForSpawn);
     }
 }

928f1c0198fc988011ad8769bb03b683698309a7
@Kubat555 <kubat.chyngyzov@gmail.com>
2022-08-02 13:17:13 +0600

    add 2 level

--- ChainGears/Assets/Scripts/ScriptableObjects/LevelInfo.cs
+++ ChainGears/Assets/Scripts/ScriptableObjects/LevelInfo.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelInfo", menuName = "ChainGears/LevelInfo")]
 public class LevelInfo : ScriptableObject {
     public List<Vector3> gearsPositions;
+    public List<Vector3> gearsRotation;
     public List<Vector3> gearsScale;
     public List<int> gearsDirection;
 }

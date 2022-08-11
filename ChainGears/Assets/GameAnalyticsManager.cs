54c9b1c9162ce8fac87f97a79a203ac1fb03ef3b
Vlad <vlad.buynyy@gmail.com>
2022-08-11 19:24:54 +0600

    Facebook and GA

---
+++ ChainGears/Assets/GameAnalyticsManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameAnalyticsSDK;
+
+public class GameAnalyticsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameAnalytics.Initialize();
+    }
+
+}
